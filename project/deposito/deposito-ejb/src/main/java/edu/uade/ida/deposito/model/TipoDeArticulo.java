e29674ff09f2a3764de5b01ffea3902d863bd786
Pablo Carle <pablo.carle@mercadolibre.com>
2016-10-31 10:14:41 -0300

    Parse method (TipoDeArticulo)

--- project/deposito/deposito-ejb/src/main/java/edu/uade/ida/deposito/model/TipoDeArticulo.java
+++ project/deposito/deposito-ejb/src/main/java/edu/uade/ida/deposito/model/TipoDeArticulo.java
@@ -9,6 +9,21 @@ public enum TipoDeArticulo {
 		this.nombre = nombre;
 	}
 	
+	public static TipoDeArticulo parse(String nombre) {
+		if (nombre is null)
+			throw new NullPointerException("nombre no puede ser null");
+		if (nombre.equalsIgnoreCase("electro"))
+			return TipoDeArticulo.ELECTRODOMESTICO;
+		else if (nombre.equalsIgnoreCase("moda"))
+			return TipoDeArticulo.MODA;
+		else if (nombre.equalsIgnoreCase("mueble"))
+			return TipoDeArticulo.MUEBLE;
+		else if (nombre.equalsIgnoreCase("niños"))
+			return TipoDeArticulo.NIÑOS;
+		else
+			throw new RuntimeException("No se reconoce tipo " + nombre);
+	}
+
 	@Override
 	public String toString() {
 		return nombre;
