7ef45a02717b2197375b6958b012773e7564f0ec
pablocarle <pablo.carle@gmail.com>
2016-11-04 19:37:12 -0300

    Avances servicio logistica y monitoreo
    Varios TODOs (ver servicio)

--- project/deposito/deposito-ejb/src/main/java/edu/uade/ida/deposito/service/out/LogisticaMonitoreoService.java
+++ project/deposito/deposito-ejb/src/main/java/edu/uade/ida/deposito/service/out/LogisticaMonitoreoService.java
@@ -1,17 +1,27 @@
 package edu.uade.ida.deposito.service.out;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.charset.Charset;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
+import javax.jms.MessageProducer;
+import javax.jms.Queue;
+import javax.jms.Session;
+import javax.jms.TextMessage;
 
 import com.google.gson.Gson;
 
@@ -33,7 +43,13 @@ public class LogisticaMonitoreoService implements LogisticaMonitoreoServiceLocal
 	@Inject
 	private Logger log;
 	
-	ConfigHolder config;
+	@Resource(mappedName = "java:/jms/queue/ColaSolicitudesArticulos") //TODO Obtener por contexto porque hay que levantar de config
+	private Queue csa;
+
+	@Resource(mappedName = "/ConnectionFactory")
+	private ConnectionFactory factory;
+	
+	ConfigHolder config; //TODO Inject
 	
     public LogisticaMonitoreoService() {
     	super();
@@ -63,21 +79,41 @@ public class LogisticaMonitoreoService implements LogisticaMonitoreoServiceLocal
 		String auditServerRestEndpointUrl = ""; //TODO Obtener config
 		MensajeAuditDTO mad = buildMensaje(nivel, mensaje);
 		String body = gson.toJson(mad);
+		log.info("Enviar sync a audit [" + body + "]");
 		try {
 			URL url = new URL(auditServerRestEndpointUrl);
 			HttpURLConnection con = (HttpURLConnection) url.openConnection();
 			con.setRequestMethod("POST");
 			con.setRequestProperty("Content-Type", "application/json");
+			OutputStream os = con.getOutputStream();
+			os.write(body.getBytes(Charset.forName("UTF-8")));
+			os.flush();
+			os.close();
+			
+			int responseCode = con.getResponseCode();
+			log.info("Envio sync a audit volvio con codigo [" + responseCode + "]");
+			
+			con.disconnect();
 		} catch (IOException e) {
 			log.log(Level.WARNING, "Error al enviar mensaje de audit sync", e);
 			e.printStackTrace();
 		}
-		
 	}
 	
 	private void enviarAuditAsync(NivelAudit nivel, String mensaje) {
-		
-		
+		try {
+			Connection con = factory.createConnection();
+			Session session = con.createSession(false, Session.AUTO_ACKNOWLEDGE);
+			con.start();
+			MessageProducer producer = session.createProducer(csa);
+			TextMessage tm = session.createTextMessage();
+			tm.setText(gson.toJson(buildMensaje(nivel, mensaje)));
+			producer.send(tm);
+			con.close();
+		} catch (JMSException e) {
+			log.log(Level.WARNING, "Error enviando audit async", e);
+			e.printStackTrace();
+		}
 	}
 	
 	private MensajeAuditDTO buildMensaje(NivelAudit nivel, String mensaje) {
---
+++ project/deposito/deposito-ejb/src/test/java/edu/uade/ida/deposito/test/integration/LogisticaMonitoreoServiceTest.java
@@ -0,0 +1,35 @@
+package edu.uade.ida.deposito.test.integration;
+
+import static org.junit.Assert.*;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(Arquillian.class)
+public class LogisticaMonitoreoServiceTest {
+
+	@Deployment
+	public static Archive<?> createDeployment() {
+		WebArchive archive = ShrinkWrap.create(WebArchive.class, "test.war").addAsWebInfResource(EmptyAsset.INSTANCE,
+				"beans.xml");
+		// System.out.println(archive.toString(true));
+		return archive;
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
