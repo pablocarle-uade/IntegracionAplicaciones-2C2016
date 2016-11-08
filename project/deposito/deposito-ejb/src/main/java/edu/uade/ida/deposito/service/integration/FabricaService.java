3e13ffd1801b0888f16cbe4f0e74995278553d89
Pablo Carle <pablo.carle@mercadolibre.com>
2016-11-08 19:14:33 -0300

    Agrego servicio fabrica vacio

---
+++ project/deposito/deposito-ejb/src/main/java/edu/uade/ida/deposito/service/integration/FabricaService.java
@@ -0,0 +1,20 @@
+package edu.uade.ida.deposito.service.integration;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class FabricaService
+ */
+@Stateless
+@LocalBean
+public class FabricaService implements FabricaServiceLocal {
+
+    /**
+     * Default constructor. 
+     */
+    public FabricaService() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
